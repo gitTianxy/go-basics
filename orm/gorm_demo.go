e372e722da08cbc8942d2f5e70b9cd0e49a220c8
gitTianxy <txy_learning@163.com>
2018-09-11 21:41:32 +0800

    gorm demo

--- orm/gorm_demo.go
+++ orm/gorm_demo.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"sync"
 )
 
 func init() {
@@ -14,63 +15,103 @@ func main() {
 	db := getDb()
 	defer db.Close()
 
-	initTbl(db)
+	dao := GetPODemoDaoInstance(db)
 
-	//db.AutoMigrate(&Person{})
+	name := "John Smith"
 
-	p1 := Person{Name: "John Smith", Age: 18}
-	p2 := Person{Name: "Jane White", Age: 20}
-
-	db.Create(&p1)
-	var p3 Person // identify a Person type for us to store the results in
-	db.First(&p3) // Find the first record in the Database and store it in p3
-
-	fmt.Println(p1.Name)
-	fmt.Println(p2.Name)
-	fmt.Println(p3.Name)
-
+	p1 := PODemo{Name: name, Age: 18}
+	fmt.Println("create:", p1)
+	dao.create(p1)
+	pdb := dao.retrieve(name)
+	fmt.Println("in db:", pdb)
+	pdb.Name = fmt.Sprintf("%s_NEW", pdb.Name)
+	dao.update(pdb)
+	fmt.Println("after update:", dao.list())
+	pdb = dao.list()[0]
+	dao.updateField(pdb.ID, "name", fmt.Sprintf("%s_NEW", pdb.Name))
+	fmt.Println("after update name:", dao.list())
+	dao.delete(pdb)
+	fmt.Println("after delete:", dao.list())
+	dao.transaction()
+	fmt.Println("after transaction", dao.list())
 }
 
 func getDb() *gorm.DB {
-	db, _ := gorm.Open("mysql", "kevin:1234/golang-basics?charset=utf8")
+	db, _ := gorm.Open("mysql", "kevin:1234@tcp(localhost:3306)/go-basics?charset=utf8&parseTime=True&loc=Local")
 	db.DB().SetMaxIdleConns(10)
 	db.DB().SetMaxOpenConns(100)
 	return db
 }
 
 func initTbl(db *gorm.DB) {
-	if !db.HasTable(&Person{}) {
-		db.CreateTable(&Person{})
+	if !db.HasTable(&PODemo{}) {
+		db.CreateTable(&PODemo{})
 		db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8").
-			CreateTable(&Person{})
+			CreateTable(&PODemo{})
 	}
 }
 
-type PersonDao struct {
+type PODemoDao struct {
 	db *gorm.DB
 }
 
-func (dao PersonDao) create(o *interface{}) {
-	dao.db.Create(o)
+func GetPODemoDaoInstance(db *gorm.DB) *PODemoDao {
+	var dao *PODemoDao
+	var once sync.Once
+	once.Do(func() {
+		initTbl(db)
+		dao = &PODemoDao{db}
+	})
+	return dao
 }
 
-func (dao PersonDao) retrieve(name string) {
-	dao.db.Model(&Person{}).Where(&Person{Name:name})
+func (dao PODemoDao) create(o PODemo) {
+	dao.db.Create(&o)
 }
 
-func (dao PersonDao) update() {
-
+func (dao PODemoDao) retrieve(name string) PODemo {
+	var p PODemo
+	dao.db.Model(&PODemo{}).Where(&PODemo{Name: name}).First(&p)
+	return p
 }
 
-func (dao PersonDao) delete() {
-
+func (dao PODemoDao) list() (pos []PODemo)  {
+	dao.db.Find(&pos)
+	return
 }
 
-func (dao PersonDao) transaction() {
-
+func (dao PODemoDao) update(po PODemo) {
+	dao.db.Save(po)
 }
 
-type Person struct {
+func (dao PODemoDao) updateField(id int, attr string, newVal interface{}) {
+	dao.db.Model(&PODemo{}).
+		Where("id=?", id).
+		Update(attr, newVal)
+}
+
+func (dao PODemoDao) delete(po PODemo) {
+	dao.db.Delete(&po)
+}
+
+func (dao PODemoDao) deleteByField(attr string, val interface{}) {
+	dao.db.Where(fmt.Sprintf("%s=?", attr), val).Delete(&PODemo{})
+}
+
+func (dao PODemoDao) transaction() (err error) {
+	tx := dao.db.Begin()
+	if err = tx.Save(&PODemo{
+		Name:"Kevin Tian",
+		Age:28,
+	}).Error; err != nil {
+		tx.Rollback()
+		return
+	}
+	tx.Commit()
+	return nil
+}
+
+type PODemo struct {
 	ID   int    `gorm:"primary_key;auto_increment"`
 	Name string `gorm:"type:varchar(20);not null;unique_index:idx_name"`
 	Age  int    `gorm:"type:int"`
